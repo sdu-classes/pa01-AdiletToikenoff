f32f5e908bf768198858f1c7598b46549dc62c89
AdiletToikenoff <118797478+AdiletToikenoff@users.noreply.github.com>
2023-02-04 13:30:01 +0600

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -1,3 +1,11 @@
-public class Circle{
-  System.out.println("Hello World");
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner io = new Scanner(System.in);
+        double a = io.nextDouble();
+        System.out.println("Area is =" + (Math.PI*a*a));
+  }
 }
